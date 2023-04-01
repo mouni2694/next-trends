93bf87f3ae24ec968a5ddb3802fe19b991416d53
mouni2694 <lavetikrishnamounica@gmail.com>
2023-04-01 17:26:43 +0200

    added local images and changed img refs in header component

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -6,12 +6,12 @@ const Header = () => (
       <div className="nav-bar-mobile-logo-container">
         <img
           class="website-logo"
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          src="/assets/images/nxt-trendz-logo-img.png"
           alt="website logo"
         />
         <button type="button" className="nav-mobile-btn">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-log-out-img.png"
+            src="/assets/images/nxt-trendz-log-out-img.png"
             alt="nav logout"
             className="nav-bar-img"
           />
@@ -21,7 +21,7 @@ const Header = () => (
       <div className="nav-content nav-bar-large-container">
         <img
           className="website-logo"
-          src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-logo-img.png"
+          src="/assets/images/nxt-trendz-logo-img.png"
           alt="website logo"
         />
         <ul className="nav-menu">
@@ -38,7 +38,7 @@ const Header = () => (
       <ul className="nav-menu-list-mobile">
         <li className="nav-menu-item-mobile">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-icon.png"
+            src="/assets/images/nxt-trendz-home-icon.png"
             alt="nav home"
             className="nav-bar-img"
           />
@@ -46,7 +46,7 @@ const Header = () => (
 
         <li className="nav-menu-item-mobile">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-icon.png"
+            src="/assets/images/nxt-trendz-products-icon.png"
             alt="nav products"
             className="nav-bar-img"
           />
@@ -54,7 +54,7 @@ const Header = () => (
 
         <li className="nav-menu-item-mobile">
           <img
-            src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-icon.png"
+            src="/assets/images/nxt-trendz-cart-icon.png"
             alt="nav cart"
             className="nav-bar-img"
           />
