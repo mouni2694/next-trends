39f4222faee010a0c7a52e754e5207d8985f2b9f
mouni2694 <lavetikrishnamounica@gmail.com>
2023-04-01 18:14:20 +0200

    created home component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header/header";
+import Home from "./components/Home/home";
 const App = () => {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 };
